11d8a1f743bbf7dc5cb70934274e980929bbdc79
ldn694 <70439334+ldn694@users.noreply.github.com>
2024-09-02 16:55:11 +0700

    Added GmailServices

--- app/src/main/java/com/cs426/asel/MainActivity.java
+++ app/src/main/java/com/cs426/asel/MainActivity.java
@@ -1,9 +1,24 @@
 package com.cs426.asel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.cs426.asel.backend.GmailServices;
+import com.cs426.asel.ui.account.UpdateAccountFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,10 +27,17 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cs426.asel.databinding.ActivityMainBinding;
+import com.google.api.services.gmail.GmailScopes;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+    implements UpdateAccountFragment.OnFragmentInteractionListener
+{
 
     private ActivityMainBinding binding;
+    private GoogleSignInClient mGoogleSignInClient;
+    private ActivityResultLauncher<Intent> signInLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +58,24 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestScopes(new Scope(GmailScopes.GMAIL_READONLY))
+                .requestIdToken(getString(R.string.client_id))
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        handleSignInResult(task);
+                    }
+                });
+
+    }
 }
