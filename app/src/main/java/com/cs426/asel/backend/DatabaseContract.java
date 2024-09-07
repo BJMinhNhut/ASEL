b806d2aef536a8539b38ba2949f490fea375e685
BJMinhNhut <nhutben678@gmail.com>
2024-09-07 23:20:31 +0700

    add database contract to prevent typo as recommended in https://developer.android.com/training/data-storage/sqlite

---
+++ app/src/main/java/com/cs426/asel/backend/DatabaseContract.java
@@ -0,0 +1,95 @@
+package com.cs426.asel.backend;
+
+import android.provider.BaseColumns;
+
+/**
+ * Database description:
+ * EVENTS(_id, title, description, from_datetime, duration, place, is_repeat, repeat_frequency, repeat_end, remind_time, all_day)
+ * MAILS(_id, summary, title, sender, receiver, send_time, event_id) id is a string
+ * TAGS(_id, name)
+ * MAIL_TAGS(mail_id, tag_id)
+ */
+
+public final class DatabaseContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private DatabaseContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class Mails implements BaseColumns {
+        public static final String TABLE_NAME = "MAILS";
+        public static final String COLUMN_NAME_SUMMARY = "summary";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SENDER = "sender";
+        public static final String COLUMN_NAME_RECEIVER = "receiver";
+        public static final String COLUMN_NAME_SEND_TIME = "send_time";
+        public static final String COLUMN_NAME_EVENT_ID = "event_id";
+
+        // SQL scripts
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " TEXT PRIMARY KEY," +
+                COLUMN_NAME_SUMMARY + " TEXT," +
+                COLUMN_NAME_TITLE + " TEXT," +
+                COLUMN_NAME_SENDER + " TEXT," +
+                COLUMN_NAME_RECEIVER + " TEXT," +
+                COLUMN_NAME_SEND_TIME + " TEXT," +
+                COLUMN_NAME_EVENT_ID + " TEXT)";
+
+        public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static class Events implements BaseColumns {
+        public static final String TABLE_NAME = "EVENTS";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_FROM_DATETIME = "from_datetime";
+        public static final String COLUMN_NAME_DURATION = "duration";
+        public static final String COLUMN_NAME_PLACE = "place";
+        public static final String COLUMN_NAME_IS_REPEAT = "is_repeat";
+        public static final String COLUMN_NAME_REPEAT_FREQUENCY = "repeat_frequency";
+        public static final String COLUMN_NAME_REPEAT_END = "repeat_end";
+        public static final String COLUMN_NAME_REMIND_TIME = "remind_time";
+        public static final String COLUMN_NAME_ALL_DAY = "all_day";
+
+        // SQL scripts
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME_TITLE + " TEXT," +
+                COLUMN_NAME_DESCRIPTION + " TEXT," +
+                COLUMN_NAME_FROM_DATETIME + " TEXT," +
+                COLUMN_NAME_DURATION + " TEXT," +
+                COLUMN_NAME_PLACE + " TEXT," +
+                COLUMN_NAME_IS_REPEAT + " INTEGER," +
+                COLUMN_NAME_REPEAT_FREQUENCY + " TEXT," +
+                COLUMN_NAME_REPEAT_END + " TEXT," +
+                COLUMN_NAME_REMIND_TIME + " TEXT," +
+                COLUMN_NAME_ALL_DAY + " INTEGER)";
+
+        public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static class Tags implements BaseColumns {
+        public static final String TABLE_NAME = "TAGS";
+        public static final String COLUMN_NAME_NAME = "name";
+
+        // SQL scripts
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME_NAME + " TEXT)";
+
+        public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static class MailTags implements BaseColumns {
+        public static final String TABLE_NAME = "MAIL_TAGS";
+        public static final String COLUMN_NAME_MAIL_ID = "mail_id";
+        public static final String COLUMN_NAME_TAG_ID = "tag_id";
+
+        // SQL scripts
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_NAME_MAIL_ID + " INTEGER," +
+                COLUMN_NAME_TAG_ID + " INTEGER)";
+
+        public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
--- app/src/main/java/com/cs426/asel/backend/DatabaseHelper.java
+++ app/src/main/java/com/cs426/asel/backend/DatabaseHelper.java
@@ -5,36 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private Context context;
-    private static final String DB_NAME = "asel";
+    private static final String DB_NAME = "ASEL.db";
     private static final int DB_VERSION = 0;
-
-    // SQL scripts
-    // create
-    private static final String CREATE_EVENTS_TABLE = "CREATE TABLE EVENTS (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, from_datetime TEXT, duration INTEGER, place TEXT, is_repeat INTEGER, repeat_frequency INTEGER, repeat_end TEXT, remind_time TEXT, all_day INTEGER)";
-    private static final String CREATE_MAILS_TABLE = "CREATE TABLE MAILS (_id TEXT PRIMARY KEY, summary TEXT, title TEXT, sender TEXT, receiver TEXT, send_time TEXT, event_id INTEGER)";
-    private static final String CREATE_TAGS_TABLE = "CREATE TABLE TAGS (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)";
-    private static final String CREATE_MAIL_TAGS_TABLE = "CREATE TABLE MAIL_TAGS (mail_id INTEGER, tag_id INTEGER)";
-
-    // drop
-    private static final String DROP_EVENTS_TABLE = "DROP TABLE IF EXISTS EVENTS";
-    private static final String DROP_MAILS_TABLE = "DROP TABLE IF EXISTS MAILS";
-    private static final String DROP_TAGS_TABLE = "DROP TABLE IF EXISTS TAGS";
-    private static final String DROP_MAIL_TAGS_TABLE = "DROP TABLE IF EXISTS MAIL_TAGS";
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
-        this.context = context.getApplicationContext();
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        updateMyDatabase(db, 0, DB_VERSION);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        updateMyDatabase(db, oldVersion, newVersion);
     }
 
     /**
@@ -45,17 +20,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * MAIL_TAGS(mail_id, tag_id)
      */
 
-    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < 1) {
-            db.execSQL(CREATE_EVENTS_TABLE);
-            db.execSQL(CREATE_MAILS_TABLE);
-            db.execSQL(CREATE_TAGS_TABLE);
-            db.execSQL(CREATE_MAIL_TAGS_TABLE);
-        }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseContract.Mails.CREATE_TABLE);
+        db.execSQL(DatabaseContract.Events.CREATE_TABLE);
+        db.execSQL(DatabaseContract.Tags.CREATE_TABLE);
+        db.execSQL(DatabaseContract.MailTags.CREATE_TABLE);
     }
 
-    MailList getMailList(int limit, String sortOption) {
-        // retreive mail list from database
-        return new MailList();
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DatabaseContract.Mails.DROP_TABLE);
+        db.execSQL(DatabaseContract.Events.DROP_TABLE);
+        db.execSQL(DatabaseContract.Tags.DROP_TABLE);
+        db.execSQL(DatabaseContract.MailTags.DROP_TABLE);
+        onCreate(db);
     }
 }
